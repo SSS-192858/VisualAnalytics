23dd3376eed61a7e0d9a35bbae365c2b87d3c8e6
siddharth-kothari9403 <siddharthvatps@gmail.com>
2023-12-09 19:56:36 +0530

    Code started

---
+++ test.py
@@ -0,0 +1,55 @@
+import pandas as pd;
+import numpy as np;
+from datetime import datetime
+
+df = pd.read_csv("datasets/Motor_Vehicle_Collisions_-_Vehicles.csv")
+df['CRASH_DATE'] = pd.to_datetime(df['CRASH_DATE'], errors='coerce')
+
+df1=df[df['CRASH_DATE']<"2013/01/01"]
+df=df[df['CRASH_DATE']>="2013/01/01"]
+df2=df[df['CRASH_DATE']<"2014/01/01"]
+df=df[df['CRASH_DATE']>="2014/01/01"]
+df3=df[df['CRASH_DATE']<"2015/01/01"]
+df=df[df['CRASH_DATE']>="2015/01/01"]
+df4=df[df['CRASH_DATE']<"2016/01/01"]
+df=df[df['CRASH_DATE']>="2016/01/01"]
+df5=df[df['CRASH_DATE']<"2017/01/01"]
+df=df[df['CRASH_DATE']>="2017/01/01"]
+df6=df[df['CRASH_DATE']<"2018/01/01"]
+df=df[df['CRASH_DATE']>="2018/01/01"]
+df7=df[df['CRASH_DATE']<"2019/01/01"]
+df=df[df['CRASH_DATE']>="2019/01/01"]
+df8=df[df['CRASH_DATE']<"2020/01/01"]
+df=df[df['CRASH_DATE']>="2020/01/01"]
+df9=df[df['CRASH_DATE']<"2021/01/01"]
+df=df[df['CRASH_DATE']>="2021/01/01"]
+df10=df[df['CRASH_DATE']<"2022/01/01"]
+df=df[df['CRASH_DATE']>="2022/01/01"]
+df11=df[df['CRASH_DATE']>="2022/01/01"]
+df12=df[df['CRASH_DATE']>="2022/01/01"]
+
+print(df1.shape)
+print(df2.shape)
+print(df3.shape)
+print(df4.shape)
+print(df5.shape)
+print(df6.shape)
+print(df7.shape)
+print(df8.shape)
+print(df9.shape)
+print(df10.shape)
+print(df11.shape)
+print(df12.shape)
+
+df1.to_csv("./datasets/vehicles_2012.csv")
+df2.to_csv("./datasets/vehicles_2013.csv")
+df3.to_csv("./datasets/vehicles_2014.csv")
+df4.to_csv("./datasets/vehicles_2015.csv")
+df5.to_csv("./datasets/vehicles_2016.csv")
+df6.to_csv("./datasets/vehicles_2017.csv")
+df7.to_csv("./datasets/vehicles_2018.csv")
+df8.to_csv("./datasets/vehicles_2019.csv")
+df9.to_csv("./datasets/vehicles_2020.csv")
+df10.to_csv("./datasets/vehicles_2021.csv")
+df11.to_csv("./datasets/vehicles_2022.csv")
+df12.to_csv("./datasets/vehicles_2023.csv")
